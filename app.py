bb13c7c085bb3a87b79cf9e118a361bae3abfa4c
ujjwalt7 <134266952+ujjwalt7@users.noreply.github.com>
2023-05-22 18:39:31 +0530

    Update app.py2

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 def home():
     return 'Hello'
 
-@app.route("/cat/<cat>/<int:page>")
+@app.route("/cat/<cat>/<page>")
 def catpage(cat,page):
     spider_name = "mem"
-    os.system("scrapy crawl "+ spider_name+ ' -a cat="'+(cat+'?page='+str(page))+'" -O output.json')
+    os.system("scrapy crawl "+ spider_name+ ' -a cat="'+cat+'?page='+page+'" -O output.json')
     # os.system("cd movies && scrapy crawl "+ spider_name+ ' -a cat="'+cat+'" -O output.json')
     with open("./output.json") as items_file:
         return json.load(items_file)
