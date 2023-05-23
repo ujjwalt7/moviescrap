1ec2d04e1fc603a3a15dc8b9c7bf5b61e67df063
ujjwalt7 <134266952+ujjwalt7@users.noreply.github.com>
2023-05-23 13:45:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,28 +4,29 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 
 app = Flask(__name__)
+os.system('pip install scrapy')
 
 @app.route('/')
 def home():
-    return 'Hello World'
+    return 'Hello World1'
 
 @app.route("/cat/<cat>/<page>")
 def catpage(cat,page):
     spider_name = "mem"
-    os.system("cd movies && scrapy crawl "+ spider_name+ ' -a cat="'+cat+'?page='+page+'" -O output.json')
+    os.system("scrapy crawl "+ spider_name+ ' -a cat="'+cat+'?page='+page+'" -O output.json')
     print(os.system('ls'))
     # os.system("cd movies && scrapy crawl "+ spider_name+ ' -a cat="'+cat+'" -O output.json')
-    with open("./movies/output.json") as items_file:
-        return json.load(items_file)
+    with open("output.json") as items_file:
+        return json.load({'items':items_file,'os':os.system('ls')})
     
 
 @app.route("/page/<type>/<link>")
 def pageHome(type,link):
     spider_name = "moviepage"
     # print("cd firstproj && scrapy "+ "crawl "+ spider_name+ " -a page='"+type+"/"+link +"' -O output.json")
-    os.system("cd movies && scrapy crawl "+ spider_name+ ' -a page="'+type+"/"+link +'" -O output.json')
+    os.system("scrapy crawl "+ spider_name+ ' -a page="'+type+"/"+link +'" -O output.json')
     # os.system("cd movies && scrapy crawl "+ spider_name+ ' -a page="'+type+"/"+link +'" -O output.json')
-    with open("./movies/output.json") as items_file:
+    with open("output.json") as items_file:
         return json.load(items_file)
     # return {'hello':'yo'}
 
